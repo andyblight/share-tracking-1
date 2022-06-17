4710befa154190419b74db0b4cfb15f7504e8656
Andy Blight <andy.blight@gmail.com>
2022-06-17 20:01:52 +0100

    GUI now uses sqlite database

--- gui_main.py
+++ gui_main.py
@@ -1,48 +1,65 @@
-# Example from http://tkdocs.com/tutorial/firstexample.html
-from tkinter import *
+# From: https://www.activestate.com/resources/quick-reads/how-to-display-data-in-a-table-using-tkinter/
+
 from tkinter import ttk
+import tkinter as tk
+import sqlite3
+
+from pathlib import Path
+
+DB_FILE_NAME = "shares.db"
+TABLE_NAME = "shares"
+
+def connect():
+    con1 = sqlite3.connect(DB_FILE_NAME)
+    cur1 = con1.cursor()
+    sql_query = "CREATE TABLE IF NOT EXISTS "
+    sql_query += TABLE_NAME
+    sql_query += "(ticker text, name text, quantity real, price real)"
+    cur1.execute(sql_query)
+    con1.commit()
+    con1.close()
+
+def add_rows():
+    connection = sqlite3.connect(DB_FILE_NAME)
+    cur = connection.cursor()
+    cur.execute("INSERT INTO shares VALUES ('FRED', 'Frederick', '12', '75.4')")
+    cur.execute("INSERT INTO shares VALUES ('BERT', 'Bertrand', '34', '175.4')")
+    cur.execute("INSERT INTO shares VALUES ('TOM', 'Thomas', '256', '275.4')")
+    connection.commit()
+    connection.close()
+
+def view():
+    con1 = sqlite3.connect(DB_FILE_NAME)
+    cur1 = con1.cursor()
+    sql_query = "SELECT * FROM " + TABLE_NAME
+    cur1.execute(sql_query)
+    rows = cur1.fetchall()
+    for row in rows:
+        print(row)
+        tree.insert("", tk.END, values=row)
+    con1.close()
 
 
-class FeetToMeters:
-    def __init__(self, root):
+# connect to the database
+db_file = Path(DB_FILE_NAME)
+if not db_file.is_file():
+    # No file so create one.
+    connect()
+    add_rows()
 
-        root.title("Feet to Meters")
+root = tk.Tk()
+tree = ttk.Treeview(root, column=("c1", "c2", "c3", "c4"), show="headings")
+tree.column("#1", anchor=tk.CENTER)
+tree.heading("#1", text="Ticker")
+tree.column("#2", anchor=tk.CENTER)
+tree.heading("#2", text="Name")
+tree.column("#3", anchor=tk.CENTER)
+tree.heading("#3", text="Quantity")
+tree.column("#4", anchor=tk.CENTER)
+tree.heading("#4", text="Price")
+tree.pack()
 
-        mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
+button1 = tk.Button(text="Display data", command=view)
+button1.pack(pady=10)
 
-        self.feet = StringVar()
-        feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
-        feet_entry.grid(column=2, row=1, sticky=(W, E))
-        self.meters = StringVar()
-
-        ttk.Label(mainframe, textvariable=self.meters).grid(
-            column=2, row=2, sticky=(W, E)
-        )
-        ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(
-            column=3, row=3, sticky=W
-        )
-
-        ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-        ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-        for child in mainframe.winfo_children():
-            child.grid_configure(padx=5, pady=5)
-
-        feet_entry.focus()
-        root.bind("<Return>", self.calculate)
-
-    def calculate(self, *args):
-        try:
-            value = float(self.feet.get())
-            self.meters.set(int(0.3048 * value * 10000.0 + 0.5) / 10000.0)
-        except ValueError:
-            pass
-
-
-root = Tk()
-FeetToMeters(root)
 root.mainloop()
