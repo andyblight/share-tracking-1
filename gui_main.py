48a96582755476a8bb3976a26cd410c39f56aac1
Andy Blight <andy.blight@gmail.com>
2022-06-18 07:59:08 +0100

    Changed to use grid layout.
    Also split out SQLite code into class.

--- gui_main.py
+++ gui_main.py
@@ -3,52 +3,70 @@
 from tkinter import ttk
 import tkinter as tk
 import sqlite3
-
 from pathlib import Path
 
-DB_FILE_NAME = "shares.db"
-TABLE_NAME = "shares"
 
-def connect():
-    con1 = sqlite3.connect(DB_FILE_NAME)
-    cur1 = con1.cursor()
-    sql_query = "CREATE TABLE IF NOT EXISTS "
-    sql_query += TABLE_NAME
-    sql_query += "(ticker text, name text, quantity real, price real)"
-    cur1.execute(sql_query)
-    con1.commit()
-    con1.close()
+class sqlite_wrapper:
+    def __init__(self):
+        self.file_name = "shares.db"
+        self.table_name = "shares"
 
-def add_rows():
-    connection = sqlite3.connect(DB_FILE_NAME)
-    cur = connection.cursor()
-    cur.execute("INSERT INTO shares VALUES ('FRED', 'Frederick', '12', '75.4')")
-    cur.execute("INSERT INTO shares VALUES ('BERT', 'Bertrand', '34', '175.4')")
-    cur.execute("INSERT INTO shares VALUES ('TOM', 'Thomas', '256', '275.4')")
-    connection.commit()
-    connection.close()
+    def create(self):
+        con1 = sqlite3.connect(self.file_name)
+        cur1 = con1.cursor()
+        sql_query = "CREATE TABLE IF NOT EXISTS "
+        sql_query += self.table_name
+        sql_query += "(ticker text, name text, quantity real, price real)"
+        cur1.execute(sql_query)
+        con1.commit()
+        con1.close()
+
+    def add_rows(self):
+        con1 = sqlite3.connect(self.file_name)
+        cur = con1.cursor()
+        cur.execute("INSERT INTO shares VALUES ('FRED', 'Frederick', '12', '75.4')")
+        cur.execute("INSERT INTO shares VALUES ('BERT', 'Bertrand', '34', '175.4')")
+        cur.execute("INSERT INTO shares VALUES ('TOM', 'Thomas', '256', '275.4')")
+        con1.commit()
+        con1.close()
+
+    def get_all_rows(self):
+        con1 = sqlite3.connect(self.file_name)
+        cur1 = con1.cursor()
+        sql_query = "SELECT * FROM " + self.table_name
+        cur1.execute(sql_query)
+        rows = cur1.fetchall()
+        con1.close()
+        return rows
+
+    def is_present(self):
+        db_file = Path(self.file_name)
+        return db_file.is_file()
+
 
 def view():
-    con1 = sqlite3.connect(DB_FILE_NAME)
-    cur1 = con1.cursor()
-    sql_query = "SELECT * FROM " + TABLE_NAME
-    cur1.execute(sql_query)
-    rows = cur1.fetchall()
-    for row in rows:
+    all_rows = database.get_all_rows()
+    for row in all_rows:
         print(row)
         tree.insert("", tk.END, values=row)
-    con1.close()
 
 
-# connect to the database
-db_file = Path(DB_FILE_NAME)
-if not db_file.is_file():
-    # No file so create one.
-    connect()
-    add_rows()
+database = sqlite_wrapper()
+# Create database if not present
+if not database.is_present():
+    database.create()
+    database.add_rows()
 
+# The window?
 root = tk.Tk()
-tree = ttk.Treeview(root, column=("c1", "c2", "c3", "c4"), show="headings")
+# The frame for everything.
+content_frame = ttk.Frame(root)
+
+# The display frame
+display_frame = ttk.Frame(content_frame, borderwidth=5, relief="ridge")
+# The table list
+treeview_frame = ttk.Frame(display_frame, borderwidth=2, relief="ridge")
+tree = ttk.Treeview(treeview_frame, column=("c1", "c2", "c3", "c4"), show="headings")
 tree.column("#1", anchor=tk.CENTER)
 tree.heading("#1", text="Ticker")
 tree.column("#2", anchor=tk.CENTER)
@@ -57,9 +75,24 @@ tree.column("#3", anchor=tk.CENTER)
 tree.heading("#3", text="Quantity")
 tree.column("#4", anchor=tk.CENTER)
 tree.heading("#4", text="Price")
-tree.pack()
+# The update button
+update_button = tk.Button(display_frame, text="Update", command=view)
 
-button1 = tk.Button(text="Display data", command=view)
-button1.pack(pady=10)
+# Data entry area
+entry_frame = ttk.Frame(content_frame)
+ticker_label = ttk.Label(entry_frame, text="Ticker")
+ticker_entry = ttk.Entry(entry_frame)
+
+# Layout items on the grid
+# Everything in here
+content_frame.grid(column=0, row=0)
+# Display frame
+display_frame.grid(column=0, row=0)
+treeview_frame.grid(column=0, row=0)
+tree.grid(column=0, row=0, columnspan=5, rowspan=4)
+update_button.grid(column=0, row=4)
+# Entry frame
+# entry_frame.grid(column=0, row=5, columnspan=5, rowspan=2)
+# ticker_label.grid
 
 root.mainloop()
