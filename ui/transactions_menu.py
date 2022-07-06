6234b4dcedd0816f3ca2544d0d38ee0e9e47ddc7
Andy Blight <andy.blight@gmail.com>
2022-07-06 13:58:01 +0100

    Can now add transaction

--- ui/transactions_menu.py
+++ ui/transactions_menu.py
@@ -65,6 +65,9 @@ class AddTransactionDialog:
         tax_label_frame.grid(column=3, row=1)
         total_label_frame.grid(column=4, row=1)
         # Buttons
+        self.validate_button = tk.Button(
+            data_entry_label_frame, text="Validate", command=self.validate
+        )
         self.add_new_button = tk.Button(
             data_entry_label_frame, text="Add", command=self.add
         )
@@ -72,26 +75,9 @@ class AddTransactionDialog:
             data_entry_label_frame, text="Cancel", command=self.cancel
         )
         # Place buttons.
-        self.add_new_button.grid(column=0, row=2)
-        self.cancel_button.grid(column=1, row=2)
-
-    def validate_date(self, date_string):
-        invalid = 0
-        try:
-            datetime.datetime.strptime(date_string, "%Y-%m-%d")
-        except (ValueError, AttributeError):
-            print("Incorrect data format, should be YYYY-MM-DD")
-            invalid = 1
-        return invalid
-
-    def validate_currency(self, value):
-        invalid = 0
-        try:
-            float(value)
-        except ValueError:
-            print("Incorrect currency format. Should be a valid number.")
-            invalid = 1
-        return invalid
+        self.validate_button.grid(column=0, row=2)
+        self.add_new_button.grid(column=1, row=2)
+        self.cancel_button.grid(column=2, row=2)
 
     def add(self):
         print("SecurityDialog Add")
@@ -104,24 +90,145 @@ class AddTransactionDialog:
         fees = self.fees_entry.get()
         tax = self.tax_entry.get()
         total = self.total_entry.get()
-        # Validate info.
-        write = 0
-        write += self.validate_date(date_entered)
-        write += self.validate_currency(price)
-        write += self.validate_currency(fees)
-        write += self.validate_currency(tax)
-        if write == 0:
-            # Write if valid.
+        # Convert values.
+        security_id = self._convert_security_id(security)
+        numerics = self._convert_numerics(quantity, price, fees, tax, total)
+        # If all values are valid, write the data.
+        if security_id[0] and numerics[0]:
             database.transactions.add_row(
-                buy_sell, date_entered, security, quantity, price, fees, tax, total
+                buy_sell,
+                date_entered,
+                security_id[1],
+                numerics[1][0],
+                numerics[1][1],
+                numerics[1][2],
+                numerics[1][3],
+                numerics[1][4],
             )
-        else:
-            print("One or more fields are invalid.")
-            # TODO Add some logic here.
+
+    def _convert_security_id(self, security_string):
+        # TODO Look up in securities table.
+        # Convert string to int.
+        valid = False
+        result = 0.0
+        try:
+            result = int(security_string)
+            valid = True
+        except ValueError:
+            print("Error. '{}' should be a valid security Id.".format(security_string))
+        return (valid, result)
+
+    def _to_float(self, value):
+        valid = False
+        result = 0.0
+        try:
+            result = float(value)
+            valid = True
+        except ValueError:
+            print("Error. '{}' should be a valid number.".format(value))
+        return (valid, result)
+
+    def _convert_numerics(self, quantity, price, fees, tax, total):
+        # print(locals())
+        # Save dictionary of parameter values.
+        values = locals()
+        # print(values)
+        # Remove self as not a float.
+        _ = values.pop("self")
+        # Convert all other values to floats.
+        results = []
+        valid = True
+        for key, value in values.items():
+            if value is not None:
+                value_result = self._to_float(value)
+                if value_result[0]:
+                    results.append(value_result[1])
+                else:
+                    print(key, value, " is not a float")
+                    valid = False
+        return (valid, results)
 
     def cancel(self):
         # Quit dialog doing nothing.
         self.dialog.destroy()
+
+    def validate(self):
+        # Get info from entry boxes.
+        date_entered = self.date_entry.get()
+        buy_sell = self.buy_sell_entry.get()
+        security = self.security_entry.get()
+        quantity = self.quantity_entry.get()
+        price = self.price_entry.get()
+        fees = self.fees_entry.get()
+        tax = self.tax_entry.get()
+        total = self.total_entry.get()
+        # Validate info.
+        write = 0
+        write += self.validate_date(date_entered)
+        write += self.validate_buy_sell(buy_sell)
+        write += self.validate_security(security)
+        write += self.validate_quantity(quantity)
+        write += self.validate_currency(price)
+        write += self.validate_currency(fees)
+        write += self.validate_currency(tax)
+        write += self.validate_total(quantity, price, fees, tax, total)
+        if write > 0:
+            print("One or more fields are invalid.")
+            # TODO Add some logic here.
+
+    def validate_buy_sell(self, buy_sell_string):
+        invalid = 0
+        # print(buy_sell_string)
+        if not buy_sell_string[0] in "BbSs":
+            print("Must be one of 'BbSs'. Given ", buy_sell_string)
+            invalid = 1
+        return invalid
+
+    def validate_currency(self, value):
+        invalid = 0
+        # TODO Improve this so there is some other sort of checking?
+        try:
+            float(value)
+        except ValueError:
+            print("Incorrect currency format. Should be a valid number.")
+            invalid = 1
+        return invalid
+
+    def validate_date(self, date_string):
+        invalid = 0
+        # print(date_string)
+        try:
+            datetime.strptime(date_string, "%Y-%m-%d")
+        except (ValueError, AttributeError):
+            print("Incorrect date format, should be YYYY-MM-DD")
+            invalid = 1
+        return invalid
+
+    def validate_security(self, security_string):
+        invalid = 0
+        # print(security_string)
+        # TODO Should check for presence in database.
+        return invalid
+
+    def validate_quantity(self, quantity_string):
+        invalid = 0
+        # print(quantity_string)
+        try:
+            float(quantity_string)
+        except ValueError:
+            print("Invalid quantity. Should be a valid number.")
+            invalid = 1
+        return invalid
+
+    def validate_total(self, quantity, price, fees, tax, total):
+        invalid = 0
+        try:
+            float(quantity_string)
+        except ValueError:
+            print("Invalid quantity. Should be a valid number.")
+            invalid = 1
+        return invalid
+        write += self.validate_total(quantity, price, fees, tax, total)
 
 
 class TransactionsTableView:
@@ -188,7 +295,7 @@ class TransactionsTableView:
         for row in all_rows:
             # print(row)
             # Convert row into correct format for treeview.
-            row_max_index = len(row) - 1
+            row_max_index = len(row)
             treeview_row = []
             for i in range(0, row_max_index):
                 # print("index", i, row[i])
@@ -202,6 +309,7 @@ class TransactionsTableView:
                     treeview_row.append(date_str)
                 else:
                     treeview_row.append(row[i])
+            print(treeview_row)
             self.tree.insert("", tk.END, values=treeview_row)
 
 
