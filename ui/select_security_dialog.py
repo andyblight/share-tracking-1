8f8fd803ec95e63b5f8dd0a66bb634ace0400dcc
Andy Blight <andy.blight@gmail.com>
2022-08-29 10:26:00 +0100

    Arranged buttons correctly on SelectSecurityDialog.
    Finally figured out the magic combination to get the grid layout to work
    correctly so that the buttons appear in sensible places.

--- ui/select_security_dialog.py
+++ ui/select_security_dialog.py
@@ -10,22 +10,29 @@ class SelectSecurityDialog:
         self.parent = parent
         self._security_id = -1
         # Set up new window.
-        self.parent = parent
         self.dialog = tk.Toplevel(self.parent)
         self.dialog.title("Add security")
-        self.dialog.geometry("500x400")
+        self.dialog.geometry("600x400")
         self.dialog.grid_rowconfigure(0, pad=10, weight=1)
         self.dialog.grid_columnconfigure(0, pad=10, weight=1)
-        # Create windows sized frame.
+        # Create window sized frame.
         self.frame = ttk.Frame(self.dialog, borderwidth=4, relief="ridge")
-        self.frame.grid(sticky="nesw")
-        self.securities_table_view = SecuritiesTableView(self.parent)
+        self.frame.grid(columnspan=5, rowspan=2, sticky="nesw")
+        # Add security table view in its own frame.
+        self.security_frame = ttk.Frame(self.frame, borderwidth=1)
+        self.securities_table_view = SecuritiesTableView(self.security_frame)
+        self.security_frame.grid(columnspan=5, column=0, row=0, sticky="ne")
+        self.securities_table_view.grid(sticky="nesw")
         # Buttons
+        self.ok_button = tk.Button(
+            self.frame, text="Ok", command=self.ok
+        )
         self.cancel_button = tk.Button(
             self.frame, text="Cancel", command=self.cancel
         )
         # Position buttons.
-        self.cancel_button.grid(column=1, row=4)
+        self.ok_button.grid(column=1, row=2)
+        self.cancel_button.grid(column=3, row=2)
 
     def get_security_id(self):
         return self._security_id
@@ -34,5 +41,9 @@ class SelectSecurityDialog:
         # Quit dialog doing nothing.
         self.dialog.destroy()
 
+    def ok(self):
+        # Quit dialog doing nothing.
+        print("SSD ok called")
+
     def top(self):
-        pass
+        self.dialog.top()
